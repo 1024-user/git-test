44fd232085b05a480b44274ac9ada2207cc35477
weijinlong <243659109@qq.com>
2021-07-06 15:37:05 +0800

    first commit

---
+++ src/main/java/com/wjl/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wjl.git;
+
+/**
+ * @author WeiJinLong
+ * @version 1.0
+ * @date 2021/7/6 15:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.txt");
+    }
+}
