40231fe80d9c9a438f2cfe25afce9548920a7287
weijinlong <243659109@qq.com>
2021-07-06 15:39:26 +0800

    second commit

--- src/main/java/com/wjl/git/GitTest.java
+++ src/main/java/com/wjl/git/GitTest.java
@@ -7,6 +7,7 @@ package com.wjl.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello.txt");
+        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
